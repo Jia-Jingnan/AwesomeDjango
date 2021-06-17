a4bd44c00c00ac9b74e5041433bb3abd5122eace
Jia-Jingnan <875480307@qq.com>
2021-06-18 03:31:53 +0800

    0618-Allowed Hosts设置可以访问的host地址

--- Alpha/Alpha/bravo/bravo/settings.py
+++ Alpha/Alpha/bravo/bravo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=@@toa&xt!dydgy9*+2ttgm&e5k)1+0*4h8rkob13w=wz(e!m7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
