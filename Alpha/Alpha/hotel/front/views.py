04c032e31751a6319d5876438f4b168d11beca1e
Jia-Jingnan <875480307@qq.com>
2021-06-18 23:35:50 +0800

    0618-在cms和front两个app中分别新增两个视图函数

--- Alpha/Alpha/hotel/front/views.py
+++ Alpha/Alpha/hotel/front/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('front首页')
+
+
+def login(request):
+    return HttpResponse('front登陆首页')
+
