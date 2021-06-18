99dbb7e6b4bfe52813f373885e6a6b2e241bef90
Jia-Jingnan <875480307@qq.com>
2021-06-18 23:44:10 +0800

    0618-修改index视图函数，如果request中含有username字段则返回，如果没有则重定向到登陆页面

--- Alpha/Alpha/hotel/front/views.py
+++ Alpha/Alpha/hotel/front/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('front首页')
+    # ?username=xxx，如果没有传usename就自动跳转到front登陆页面
+    username = request.GET.get('username')
+    if username:
+        return HttpResponse('front首页')
+    else:
+        return redirect("/login/")
 
 
 def login(request):
