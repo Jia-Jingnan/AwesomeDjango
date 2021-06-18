0ff786b588e3b923f58519995fe12376a8951eca
Jia-Jingnan <875480307@qq.com>
2021-06-18 23:12:32 +0800

    0618-新增视图函数

--- Alpha/Alpha/golf/book/views.py
+++ Alpha/Alpha/golf/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def book(request):
+    return HttpResponse("图书首页")
+
+
+def book_detail(request, book_id):
+    text = '图书的ID是：%s' % book_id
+    return HttpResponse(text)
