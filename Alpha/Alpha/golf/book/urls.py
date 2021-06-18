adffa04b9e32097746a4163f3212d1b70b1a2eb9
Jia-Jingnan <875480307@qq.com>
2021-06-18 23:17:38 +0800

    0618-将视图函数的url写在app模块下的urls.py文件中，并在在项目目录下的urls.py指向该urls.py

---
+++ Alpha/Alpha/golf/book/urls.py
@@ -0,0 +1,11 @@
+# coding=UTF-8
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    # book/
+    path('', views.book),
+    # book/detail/1
+    path('detail/<book_id>', views.book_detail)
+]
