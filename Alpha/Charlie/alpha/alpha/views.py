b51347a9db0f1dd03fe61dbd8faba72d557e65d3
Jia-Jingnan <875480307@qq.com>
2021-06-20 23:42:44 +0800

    0620-使用cursor游标对象执行原生SQL语句

--- Alpha/Charlie/alpha/alpha/views.py
+++ Alpha/Charlie/alpha/alpha/views.py
@@ -3,7 +3,16 @@
 # Desc:
 
 from django.shortcuts import render
+from django.db import connection
 
 
 def index(request):
+
+    # 获取游标
+    cursor = connection.cursor()
+    # cursor.execute("insert into book(id, name, author) values (null, '西游记','吴承恩')")
+    cursor.execute("select id, name, author from book")
+    rows = cursor.fetchall()  # 返回全部数据
+    for row in rows:
+        print(row)
     return render(request, 'index.html')
