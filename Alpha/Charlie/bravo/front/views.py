3eeadf0600cc2b7479fa300754d4c5c094bef3b1
Jia-Jingnan <875480307@qq.com>
2021-06-21 00:01:55 +0800

    0620-完善index视图函数

--- Alpha/Charlie/bravo/front/views.py
+++ Alpha/Charlie/bravo/front/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.db import connection
+
+
+def get_cursor():
+    return connection.cursor()
 
 
 # Create your views here.
 # 视图函数
 def index(request):
-    pass
+    cursor = get_cursor()
+    cursor.execute('select id, name, author from book')
+    books = cursor.fetchall()  # 返回的是一个列表
+    # 准备上下文
+    context = {
+        'books': books
+    }
+    return render(request, 'index.html', context=context)
 
 
 def add_book(request):
