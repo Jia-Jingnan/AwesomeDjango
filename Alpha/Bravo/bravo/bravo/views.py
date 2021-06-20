fc9fa2ac77ee1eeb887772b5eea1bdd2a60863c1
Jia-Jingnan <875480307@qq.com>
2021-06-20 21:30:34 +0800

    0620-context为上下文，context中存储可以在html中展示的数据

--- Alpha/Bravo/bravo/bravo/views.py
+++ Alpha/Bravo/bravo/bravo/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    # context用来存储上下文参数，既可以在html中展示的变量都可以在context中定义
+    # context为字典类型
+    context = {
+        'username': 'bravo'
+    }
+    return render(request, 'index.html', context=context)
