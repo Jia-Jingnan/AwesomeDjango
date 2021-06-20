a6a65a06a48820294fc0661a8bfb5593b565bbbe
Jia-Jingnan <875480307@qq.com>
2021-06-20 23:55:14 +0800

    0620-在urls中新增视图函数的映射

--- Alpha/Charlie/bravo/bravo/urls.py
+++ Alpha/Charlie/bravo/bravo/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from front import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('book_detail/', views.book_detail, name='book_detail')
 ]
