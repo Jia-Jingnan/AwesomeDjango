120e780746426a372ab46e1d5d9a291f0275d8c2
Jia-Jingnan <875480307@qq.com>
2021-06-20 23:25:16 +0800

    0620-修改数据库配置信息，并在本地新建local_settings.py配置文件，在settings.py中导入，在local_settings中更新数据库配置信息

--- Alpha/Charlie/alpha/alpha/settings.py
+++ Alpha/Charlie/alpha/alpha/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '1qaz2wsx',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
@@ -119,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from . import local_settings
+except ImportError:
+    pass
