d3c47ace50da8bdf943729365ee0e4e646908e69
Jia-Jingnan <875480307@qq.com>
2021-06-20 23:54:44 +0800

    0620-新增三个视图函数框架

--- Alpha/Charlie/bravo/front/views.py
+++ Alpha/Charlie/bravo/front/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# 视图函数
+def index(request):
+    pass
+
+
+def add_book(request):
+    pass
+
+
+def book_detail(request, book_id):
+    pass
