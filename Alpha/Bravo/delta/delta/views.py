e260f5316529de69ce0238b3449c29cf890f5fb1
Jia-Jingnan <875480307@qq.com>
2021-06-23 01:33:49 +0800

    0623-for循环组成一个表格

--- Alpha/Bravo/delta/delta/views.py
+++ Alpha/Bravo/delta/delta/views.py
@@ -8,11 +8,21 @@ from django.shortcuts import render
 def index(request):
     context = {
         'books':[
-            'SpriderMan',
-            'IronMan',
-            'Doctor Stranger',
-            'Thor',
-            'Captain American'
+            {
+                'name': '三国演绎',
+                'author': '罗贯中',
+                'price': '199'
+            },
+            {
+                'name': '水浒传',
+                'author': '施耐庵',
+                'price': '109'
+            },
+            {
+                'name': '西游记',
+                'author': '吴承恩',
+                'price': '109'
+            },
         ],
         'person':{
             'username': 'zhiliao',
