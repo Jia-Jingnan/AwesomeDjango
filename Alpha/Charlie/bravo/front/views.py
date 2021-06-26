5badcac82c740e0ae4880db2f95edc52f4d8f0c9
Jia-Jingnan <875480307@qq.com>
2021-06-26 10:00:15 +0800

    0625-完成简单的图书管理系统

--- Alpha/Charlie/bravo/front/views.py
+++ Alpha/Charlie/bravo/front/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.db import connection
 
 
@@ -20,8 +20,29 @@ def index(request):
 
 
 def add_book(request):
-    pass
+    if request.method == 'GET':
+        return render(request, 'add_book.html')
+    else:
+        name = request.POST.get('name')
+        author = request.POST.get('author')
+        cursor = get_cursor()
+        cursor.execute("insert into book(id, name, author) values (null, '%s', '%s')" % (name, author))
+        return redirect(reverse('index'))
 
 
 def book_detail(request, book_id):
-    pass
+    cursor = get_cursor()
+    cursor.execute("select id, name, author from book where id = %s" % book_id)
+    book = cursor.fetchone()
+    context = {'book': book}
+    return render(request, 'book_detail.html', context)
+
+
+def delete_book(request):
+    if request.method == 'POST':
+        book_id = request.POST.get('book_id')
+        cursor = get_cursor()
+        cursor.execute("delete from book where id = %s" % book_id)
+        return redirect(reverse('index'))
+    else:
+        raise RuntimeError('删除图书的方法错误')
